811d411656c70ceba1b8f09d2ab4cf87b271d2de
Srinivas <srinu9519@gmail.com>
2024-03-17 14:21:07 +0530

    First Commit

---
+++ Basics.py
@@ -0,0 +1,14 @@
+#Sum of 2 Numbers
+
+num1 = int(input("Enter Num 1 "))
+num2 = int(input("Enter Num 2 "))
+sum_result =num1 + num2;
+print(f"Sum of two numbers {sum_result}");
+
+# Prints today's date with help
+# of datetime library
+import datetime
+ 
+today = datetime.datetime.today()
+print(f"{today:%B %d, %Y}")
+
